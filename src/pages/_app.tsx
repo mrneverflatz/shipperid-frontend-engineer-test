c64d60f28468fd690c820afa07f040b16c82797c
Yein Narayana <mrneverflatz@gmail.com>
2022-07-28 12:17:45 +0800

    cancel routing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import "assets/styles/globals.css";
 import type { AppProps } from "next/app";
+import Router from "next/router";
 
 import store from "lib/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 import { Provider } from "react-redux";
+
+Router.ready(() => {
+  Router.events.on("routeChangeStart", () => {});
+});
+
 const persistor = persistStore(store);
 
 function MyApp({ Component, pageProps }: AppProps) {
